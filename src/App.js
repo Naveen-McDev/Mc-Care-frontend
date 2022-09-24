9c2c7693c6701b39ed7114e8f2feb2711d87ec4a
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-09-24 18:09:35 +0530

    Login and registration UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mc- Care</h1>
-    </div>
+    <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Register.js
@@ -0,0 +1,69 @@
+import { Button, Form, Input } from "antd";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+function Register() {
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/api/user/register`,
+        values
+      );
+      if (response.data.success) {
+        toast.success(response.data.message);
+        navigate("/login");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="authentication">
+      <div className="authentication-form card p-3">
+        <h1 className="card-title">Hello..!</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input placeholder="Name" type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input placeholder="Email" type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <Button
+            className="primary-button my-2 full-width-button"
+            htmlType="submit"
+          >
+            REGISTER
+          </Button>
+
+          <Link to="/login" className="anchor mt-2">
+            CLICK HERE TO LOGIN
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
